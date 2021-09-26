03e371fbeb5fa78bcccfbd9bc96c93d452451e59
noamhoffer <noamhoffer@gmail.com>
2021-09-26 11:29:49 +0300

    mendelssohn

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-/d/apps/hugo.exe # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo.exe # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
